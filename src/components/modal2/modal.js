dbef7698fc17fc716405d4d3fe1adaa6e53d8535
NazarDzneladze <nazar.dz055@mail.ru>
2022-10-11 11:18:01 +0300

    Доьавил модалку для кнопки пожаловаться

--- src/components/modal2/modal.js
+++ src/components/modal2/modal.js
@@ -1,98 +1,147 @@
 import { Button } from "../common/button";
 
 export function Modal2() {
-  this.rootElement = document.createElement("div");
-  this.rootElement.classList.add("modal", "fade");
-  this.rootElement.id = "exampleModal2";
-  this.rootElement.tabIndex = "-1";
-  this.rootElement.ariaHidden = "true";
-  this.rootElement.setAttribute("aria-labelledby", "exampleModalLabel2");
 
-  this.modalDialog = document.createElement("div");
-  this.modalDialog.classList.add("modal-dialog");
 
-  this.modalContent = document.createElement("div");
-  this.modalContent.classList.add("modal-content");
+	this.rootElement = document.createElement("div");
+	this.rootElement.classList.add("modal", "fade");
+	this.rootElement.id = "exampleModal2";
+	this.rootElement.tabIndex = "-1";
+	this.rootElement.ariaHidden = "true";
+	this.rootElement.setAttribute("aria-labelledby", "exampleModalLabel2");
 
-  this.modalHeader = document.createElement("div");
-  this.modalHeader.classList.add("modal-header");
+	this.modalDialog = document.createElement("div");
+	this.modalDialog.classList.add("modal-dialog");
 
-  this.modalTitle = document.createElement("h1");
-  this.modalTitle.classList.add("modal-title", "fs-5");
-  this.modalTitle.id = "exampleModalLabel2";
-  this.modalTitle.innerText = "Пожаловаться на пин";
+	this.modalContent = document.createElement("div");
+	this.modalContent.classList.add("modal-content");
 
-  this.modalBtn = new Button({ className: "btn-close" });
-  this.modalBtn.rootElement.setAttribute("data-bs-dismiss", "modal");
-  this.modalBtn.rootElement.setAttribute("aria-label", "Close");
+	this.modalHeader = document.createElement("div");
+	this.modalHeader.classList.add("modal-header");
 
-  this.modalBody = document.createElement("div");
-  this.modalBody.classList.add("modal-body");
+	this.modalTitle = document.createElement("h1");
+	this.modalTitle.classList.add("modal-title", "fs-5");
+	this.modalTitle.id = "exampleModalLabel2";
+	this.modalTitle.innerText = "Пожаловаться на пин";
 
-  this.modalCheck1 = document.createElement("input");
-  this.modalCheck1.type = "checkbox";
-  this.modalCheck1.innerText = "Спам";
+	this.modalBtn = new Button({ className: "btn-close" });
+	this.modalBtn.rootElement.setAttribute("data-bs-dismiss", "modal");
+	this.modalBtn.rootElement.setAttribute("aria-label", "Close");
 
-  this.modalCheck2 = document.createElement("input");
-  this.modalCheck2.type = "checkbox";
-  this.modalCheck2.innerText = "Изображения обнажённого тела или порнография";
+	this.modalBody = document.createElement("div");
+	this.modalBody.classList.add("modal-body");
 
-  this.modalCheck3 = document.createElement("input");
-  this.modalCheck3.type = "checkbox";
-  this.modalCheck3.innerText = "Членовредительство";
+	// Первый чекбокс
+	this.firstCheckbox = document.createElement('div');
+	this.firstLabel = document.createElement('label');
+	this.modalCheck1 = document.createElement("input");
+	this.modalCheck1.type = "checkbox";
+	this.firstLabel.innerText = "Спам";
+	this.firstCheckbox.append(this.modalCheck1, this.firstLabel);
+	// Первый чекбокс
 
-  this.modalCheck4 = document.createElement("input");
-  this.modalCheck4.type = "checkbox";
-  this.modalCheck4.innerText = "Ложная информация";
 
-  this.modalCheck5 = document.createElement("input");
-  this.modalCheck5.type = "checkbox";
-  this.modalCheck5.innerText = "Агрессивные действия";
+	// Второй чекбокс
+	this.secondCheckbox = document.createElement('div');
+	this.secondLabel = document.createElement('label');
+	this.modalCheck2 = document.createElement("input");
+	this.modalCheck2.type = "checkbox";
+	this.secondLabel.innerText = "Изображения обнажённого тела или порнография";
+	this.secondCheckbox.append(this.modalCheck2, this.secondLabel);
+	// Второй чекбокс
 
-  this.modalCheck6 = document.createElement("input");
-  this.modalCheck6.type = "checkbox";
-  this.modalCheck6.innerText = "Опасные товары";
+	// Третий чекбокс
+	this.thirdCheckbox = document.createElement('div');
+	this.thirdLabel = document.createElement('label');
+	this.modalCheck3 = document.createElement("input");
+	this.modalCheck3.type = "checkbox";
+	this.thirdLabel.innerText = "Членовредительство";
+	this.thirdCheckbox.append(this.modalCheck3, this.thirdLabel);
+	// Третий чекбокс
 
-  this.modalCheck7 = document.createElement("input");
-  this.modalCheck7.type = "checkbox";
-  this.modalCheck7.innerText = "Сцены насилия";
+	// Четвертый чекбокс
+	this.fourthCheckbox = document.createElement('div');
+	this.fourthLabel = document.createElement('label');
+	this.modalCheck4 = document.createElement("input");
+	this.modalCheck4.type = "checkbox";
+	this.fourthLabel.innerText = "Ложная информация";
+	this.fourthCheckbox.append(this.modalCheck4, this.fourthLabel);
+	// Четвертый чекбокс
 
-  this.modalCheck8 = document.createElement("input");
-  this.modalCheck8.type = "checkbox";
-  this.modalCheck8.innerText = "Это моя интеллектуальная собственность";
+	// Пятый чекбокс
+	this.fifthCheckbox = document.createElement('div');
+	this.fifthLabel = document.createElement('label');
+	this.modalCheck5 = document.createElement("input");
+	this.modalCheck5.type = "checkbox";
+	this.fifthLabel.innerText = "Агрессивные действия";
+	this.fifthCheckbox.append(this.modalCheck5, this.fifthLabel);
+	// Пятый чекбокс
 
-  this.modalBody.append(
-    this.modalCheck1,
-    this.modalCheck2,
-    this.modalCheck3,
-    this.modalCheck4,
-    this.modalCheck5,
-    this.modalCheck6,
-    this.modalCheck7,
-    this.modalCheck8
-  );
+	// Шестой чекбокс
+	this.sixthCheckbox = document.createElement('div');
+	this.sixthLabel = document.createElement('label');
+	this.modalCheck6 = document.createElement("input");
+	this.modalCheck6.type = "checkbox";
+	this.sixthLabel.innerText = "Опасные товары";
+	this.sixthCheckbox.append(this.modalCheck6, this.sixthLabel);
+	// Шестой чекбокс
 
-  this.modalFooter = document.createElement("div");
-  this.modalFooter.classList.add("modal-footer");
+	// Седьмой чекбокс
+	this.seventhCheckbox = document.createElement('div');
+	this.seventhLabel = document.createElement('label');
+	this.modalCheck7 = document.createElement("input");
+	this.modalCheck7.type = "checkbox";
+	this.seventhLabel.innerText = "Сцены насилия";
+	this.seventhCheckbox.append(this.modalCheck7, this.seventhLabel);
+	// Седьмой чекбокс
 
-  this.buttonCancel = new Button({
-    label: "Отмена",
-    className: "button-modal-cancel",
-  });
-  this.buttonOwnward = new Button({
-    label: "Далее",
-    className: "button-modal-ownward",
-  });
+	// Восьмой чекбокс
+	this.eighthCheckbox = document.createElement('div');
+	this.eighthLabel = document.createElement('label');
+	this.modalCheck8 = document.createElement("input");
+	this.modalCheck8.type = "checkbox";
+	this.eighthLabel.innerText = "Это моя интеллектуальная собственность";
+	this.eighthCheckbox.append(this.modalCheck8, this.eighthLabel);
+	// Восьмой чекбокс
 
-  this.buttonsBox.append(
-    this.buttonCancel.rootElement,
-    this.buttonOwnward.rootElement
-  );
 
-  this.modalBody.append(this.buttonsBox);
-  this.modalHeader.append(this.modalTitle, this.modalBtn.rootElement);
-  this.modalContent.append(this.modalHeader, this.modalBody);
-  this.modalDialog.append(this.modalContent);
+	this.modalBody.append(
+		this.firstCheckbox,
+		this.secondCheckbox,
+		this.thirdCheckbox,
+		this.fourthCheckbox,
+		this.fifthCheckbox,
+		this.sixthCheckbox,
+		this.seventhCheckbox,
+		this.eighthCheckbox,
+	);
 
-  this.rootElement.append(this.modalDialog);
+
+	this.modalFooter = document.createElement("div");
+	this.modalFooter.classList.add("modal-footer");
+
+	this.buttonCancel = new Button({
+		label: "Отмена",
+	});
+	this.buttonCancel.rootElement.classList.add('btn', 'btn-secondary');
+	this.buttonCancel.rootElement.setAttribute('data-bs-dismiss', 'modal');
+
+	this.buttonOwnward = new Button({
+		label: "Далее",
+	});
+	this.buttonOwnward.rootElement.classList.add('btn', 'btn-primary');
+
+	this.buttonsBox = document.createElement('div');
+
+	this.buttonsBox.append(
+		this.buttonCancel.rootElement,
+		this.buttonOwnward.rootElement,
+	);
+
+	this.modalBody.append(this.buttonsBox);
+	this.modalHeader.append(this.modalTitle, this.modalBtn.rootElement);
+	this.modalContent.append(this.modalHeader, this.modalBody);
+	this.modalDialog.append(this.modalContent);
+
+	this.rootElement.append(this.modalDialog);
 }
